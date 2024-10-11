a1d19bdc99c135370d276beb40be38985b6db302
Carmy93 <carmygargiulo45@gmail.com>
2024-10-11 11:29:29 +0200

    enums created

---
+++ src/main/java/CarmineGargiulo/enums/Genre.java
@@ -0,0 +1,5 @@
+package CarmineGargiulo.enums;
+
+public enum Genre {
+    FANTASY, ROMANCE, ADVENTURE, SCIENCE_FICTION, HORROR, MYSTERY
+}
---
+++ src/main/java/CarmineGargiulo/enums/Peridiocity.java
@@ -0,0 +1,5 @@
+package CarmineGargiulo.enums;
+
+public enum Peridiocity {
+    WEEKLY, MONTHLY, SEMI_ANNUALLY
+}
